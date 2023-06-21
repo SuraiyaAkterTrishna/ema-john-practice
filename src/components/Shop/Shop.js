5e1d3cfbb117adeaf710b76c1ae285c14c3e439e
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-21 15:38:56 +0600

    shop component layout done

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h1>Here is all products</h1>
+            </div>
+            <div className='cart-container'>
+                <h1>This is cart container</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
